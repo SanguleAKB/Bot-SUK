c7998af2776576066c29433f7656edf82e48ba47
Aniruddha Sangule <aniruddhasangule2001@gmail.com>
2025-03-06 06:08:31 +0530

    added gunicon

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def chat():
     return jsonify({"bot_response": "Please provide a valid question."})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(debug=True)
